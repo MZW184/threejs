d4e514662185ebd08efdee84985832a0aff2a68b
MZW184 <134600467+MZW184@users.noreply.github.com>
2024-09-06 16:36:04 +0200

    updated cv file with html css and js

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+let isShaking = false;
+const nameElement = document.querySelector(".name");
+
+function startShaking() {
+  if (!isShaking) {
+    nameElement.classList.add("shake");
+    isShaking = true;
+  }
+}
+
+function stopShaking() {
+  if (isShaking) {
+    nameElement.classList.remove("shake");
+    isShaking = false;
+  }
+}
+
+const codeVoyagerHeading = document.getElementById("codeVoyager");
+const originalText = "Code Voyager";
+const transformedText = "Frontend Developer";
+
+function toggleText() {
+  if (codeVoyagerHeading.textContent === originalText) {
+    codeVoyagerHeading.textContent = transformedText;
+  } else {
+    codeVoyagerHeading.textContent = originalText;
+  }
+}
+
+codeVoyagerHeading.addEventListener("mouseenter", toggleText);
+codeVoyagerHeading.addEventListener("mouseleave", toggleText);
+
+codeVoyagerHeading.style.transition = "color 0.5s ease";
